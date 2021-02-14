785798b9f6c19c28001394e721ba73f388b71d6c
RJ Salvador <rjsalvadorr@gmail.com>
2021-02-14 11:33:15 -0500

    fixing styles

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -9,9 +9,11 @@ type Props = {
 
 const Content = ({ body, title, galleryView }: Props) => {
   const extraClass = galleryView ? styles['content--gallery'] : null;
+  const extraClass2 = galleryView ? styles['content--gallery__title'] : null;
+
   return (
   <div className={`${styles['content']} ${extraClass}`}>
-    <h1 className={styles['content__title']}>{title}</h1>
+    <h1 className={`${styles['content__title']} ${extraClass2}`}>{title}</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 )};
